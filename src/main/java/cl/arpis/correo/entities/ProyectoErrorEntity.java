8b23a6f1eb9bd93e8de3ba89cb4809073f1c3da6
Steph <proyectos@arpis.cl>
2022-08-06 02:44:02 -0400

    modificación query correos de error

--- src/main/java/cl/arpis/correo/entities/ProyectoErrorEntity.java
+++ src/main/java/cl/arpis/correo/entities/ProyectoErrorEntity.java
@@ -1,6 +1,7 @@
 package cl.arpis.correo.entities;
 
 import java.io.Serializable;
+import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -10,7 +11,7 @@ import javax.persistence.Table;
 import lombok.Builder;
 
 @Entity
-@Table(name = "API_CC_TIPO_ERROR")
+@Table(name = "API_CC_PROYECTO_ERROR")
 @Builder
 public class ProyectoErrorEntity implements Serializable{
 
@@ -25,4 +26,17 @@ public class ProyectoErrorEntity implements Serializable{
 
 	@Column(name = "ID_TIPO_ERROR", nullable = false)
 	private Long idTipoError;
+	
+	@Column(name = "ID_PROYECTO", nullable = false)
+	private Long idProyecto;
+
+	@Column(name = "ID_CORREO", nullable = false)
+	private Long idCorreo;
+
+	@Column(name = "ID_TIPO_ENVIO", nullable = false)
+	private Long idTipoEnvio;
+	
+	@Column(name = "FECHA_REACION", nullable = false)
+	private Date fechaCreacion;
+	
 }
